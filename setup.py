89c65c3690bde008bc5e456165a7b633c50fa04b
ShahRustam <shahrustam98@gmail.com>
2016-12-13 23:53:08 +0200

    test 5

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ setup(
     version='1.0.0',
     description='Python client for JetBrains\' YouTrack REST API',
     packages=[
-    'agilezen', 'agilezen2youtrack.py', 'bitbucket-broker.py', 'bugzilla', 'bugzilla2youtrack.py', 'build.xml',
-    'copyOneIssue.py', 'csv2youtrack.py', 'csvClient', 'deleteAllUsers.py', 'fb2youtrack.py', 'fbugz',
-    'getAllEmailsFromProject.py', 'github2youtrack.py', 'googleCode', 'googleCode2youtrack.py', 'httplib2', 'jira',
-    'jira2youtrack.py', 'jiraSOAP2youtrack.py', 'mantis', 'mantis2youtrack.py', 'moveIssue.py', 'out',
-    'pyactiveresource', 'Python.iml', 'redmine', 'redmine2youtrack.py', 'socks.py', 'sync', 'syncYtWithYt.py',
-    'sync_config', 'test.py', 'tests', 'trac2youtrack.py', 'tracLib', 'urllib2_file.py', 'userTool.py', 'youtrack',
-    'youtrack2youtrack.py', 'youtrackImporter.py', 'youtrackRestSamples.py', 'zendesk', 'zendesk2youtrack.py']
+        'python', 'python.agilezen',
+        'python.bugzilla', 'python.csvClient',
+        'python.fbugz', 'python.googleCode', 'python.httplib2', 'python.jira', 'python.mantis',
+        'python.out', 'python.pyactiveresource', 'python.redmine', 'python.sync', 'python.tests',
+        'python.tracLib', 'python.youtrack', 'python.zendesk'
+    ],
 )
